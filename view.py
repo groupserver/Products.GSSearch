12f1308bdd6dca99fe2006f9d12b074ac39f9cb6
Michael JasonSmith <mpj17@onlinegroups.net>
2007-05-07 04:49:32 +0000

    API documentation, the results page now uses the standard skin, the page-title is now correct, and topic-only searching goes.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSSearch/trunk%402048

--- view.py
+++ view.py
@@ -13,6 +13,111 @@ class GSSearchView(BrowserView):
         self.searchText = self.request.get('searchText', '')
         self.groupId = self.request.get('groupId', '')
 
+        self.startIndex = self.request.get('startIndex', 0)
+
+    def get_title(self):
+        retval = ''
+        
+        in_ = []
+        if self.view_topics():
+            in_.append('topics')
+        if self.view_posts():
+            in_.append('posts')
+        if self.view_files():
+            in_.append('files')
+        if self.view_profiles():
+            in_.append('profiles')
+        
+        if len(in_) > 1:
+            inStr = ', '.join(in_[:-1])
+            inStr = '%s and %s' % (inStr, in_[-1])
+        else:
+            inStr = in_[0]
+        grp = ''
+        if self.groupId:
+            grp = ' :%s' % self.groupId
+            
+        r = r'Search for %s in %s%s: %s'
+        retval = r % (self.searchText, inStr, grp, 
+                      self.siteInfo.get_name())
+        
+        return retval
+
+    def __get_boolean(self, var, default=True):
+        """Get a Boolean value from the form
+        
+        Getting a Boolean value from an HTML form is a stupidly
+        difficult task. To save sanity, this method performs the
+        leg-work.
+        
+        ARGUMENTS
+            var:     A string naming the Boolean variable to get
+            default: The default value for var, if not present.
+            
+        RETURNS
+            The value of "var" in the form, or the value of "default" 
+            if "var" is not set in the form.
+        """
+        val = self.request.get(var, default)
+        try:
+            # since accept should be '1' if set
+            val = int(val) and True or False
+        except ValueError:
+            val = default
+        return val
+    
+    def view_topics(self):
+        return self.__get_boolean('viewTopics', True)
+
+    def only_topics_shown(self):
+        topics = self.__get_boolean('viewTopics', True)
+        posts = self.__get_boolean('viewPosts', False)
+        files = self.__get_boolean('viewFiles', True)
+        profiles = self.__get_boolean('viewProfiles', True)
+        return topics and not(posts or files or profiles)
+
+    def only_topics_link(self):
+        group = self.groupId and 'groupId=%s&' % self.groupId or ''
+        s = r'%s?searchText=%s%s&viewTopics=1&viewFiles=0&viewProfiles=0'
+        retval = s % (self.request.URL, self.searchText, group)
+        return retval
+
+    def view_posts(self):
+        return self.__get_boolean('viewPosts', False)
+
+    def only_posts_shown(self):
+        topics = self.__get_boolean('viewTopics', True)
+        posts = self.__get_boolean('viewPosts', False)
+        files = self.__get_boolean('viewFiles', True)
+        profiles = self.__get_boolean('viewProfiles', True)
+        return posts and not(topics or files or profiles)
+
+    def only_posts_link(self):
+        group = self.groupId and 'groupId=%s&' % self.groupId or ''
+        s = r'%s?searchText=%s%s&viewTopics=0&viewFiles=0&viewProfiles=1'
+        retval = s % (self.request.URL, self.searchText, group)
+        return retval
+
+    def view_files(self):
+        return self.__get_boolean('viewFiles', True)
+    
+    def only_files_shown(self):
+        topics = self.__get_boolean('viewTopics', True)
+        posts = self.__get_boolean('viewPosts', False)
+        files = self.__get_boolean('viewFiles', True)
+        profiles = self.__get_boolean('viewProfiles', True)
+        return files and not(topics or posts or profiles)
+
+    def view_profiles(self):
+        return self.__get_boolean('viewProfiles', True)
+
+    def only_profiles_shown(self):
+        topics = self.__get_boolean('viewTopics', True)
+        posts = self.__get_boolean('viewPosts', False)
+        files = self.__get_boolean('viewFiles', True)
+        profiles = self.__get_boolean('viewProfiles', True)
+        return profiles and not(topics or posts or files)
+            
     def process_form(self):
         form = self.context.REQUEST.form
         result = {}
