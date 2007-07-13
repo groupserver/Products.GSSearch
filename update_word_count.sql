b53b1ff90ce1b10f0dac0a26fd90557799f8c144
Michael JasonSmith <mpj17@onlinegroups.net>
2007-07-13 00:35:27 +0000

    SQL script for updating the word_count table.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSSearch/trunk%402258

---
+++ update_word_count.sql
@@ -0,0 +1,10 @@
+-- Update script for the word_count table
+
+-- Yes, we do intend to delete all the records from
+--   the word_count table; it is faster this way.
+DELETE FROM word_count;
+
+-- Aggregate the data from the topic_word_count table.
+INSERT INTO word_count SELECT word, SUM(count) 
+FROM topic_word_count GROUP BY word;
+
