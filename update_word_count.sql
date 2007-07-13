21709268750f8a51b01bf9b47f553b45a9f84453
Michael JasonSmith <mpj17@onlinegroups.net>
2007-07-13 01:36:06 +0000

    Added a transaction block.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSSearch/trunk%402259

--- update_word_count.sql
+++ update_word_count.sql
@@ -1,5 +1,5 @@
 -- Update script for the word_count table
-
+BEGIN;
 -- Yes, we do intend to delete all the records from
 --   the word_count table; it is faster this way.
 DELETE FROM word_count;
@@ -7,4 +7,5 @@ DELETE FROM word_count;
 -- Aggregate the data from the topic_word_count table.
 INSERT INTO word_count SELECT word, SUM(count) 
 FROM topic_word_count GROUP BY word;
+COMMIT;
 
