970e9334afba800b6f1e8227b34bb993eed10504
Richard Waid <richard@iopen.net>
2007-08-10 11:54:09 +0000

    Rework to hopefully use the cursor rather than relying on database limiting since we want the count anyway
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSSearch/trunk%402457

--- queries.py
+++ queries.py
@@ -109,8 +109,15 @@ class MessageQuery(Products.XWFMailingListManager.queries.MessageQuery):
         
     def topic_search_keyword(self, searchTokens, site_id, 
         group_ids=[], limit=12, offset=0):
-        """Search for the search text in the content and subject-lines of
-        topics"""
+        """ Search for the search text in the content and subject-lines of
+        topics.
+            
+            Returns: (result_batch, total_count, batch_count)
+            
+            total_count is the count of all results matching the search,
+            regardless of their involvement in the result_batch, batch_count
+            is the count of results in the batch.
+        """
         tt = self.topicTable
         pt = self.postTable
         wct = self.topic_word_countTable
@@ -126,23 +133,26 @@ class MessageQuery(Products.XWFMailingListManager.queries.MessageQuery):
           self.topicTable,  site_id, group_ids)
         statement = self.__add_topic_keyword_search_where_clauses(statement, 
           searchTokens)
-        statement.limit = limit
-        statement.offset = offset
+        #statement.limit = limit
+        #statement.offset = offset
         statement.order_by(sa.desc(tt.c.last_post_date))
         
         r = statement.execute()
-        retval = []
-        for x in r:
-            retval.append(
-                {'topic_id': x['topic_id'],
-                 'last_post_id': x['last_post_id'], 
-                 'first_post_id': x['first_post_id'], 
-                 'group_id': x['group_id'], 
-                 'site_id': x['site_id'], 
-                 'subject': unicode(x['original_subject'], 'utf-8'), 
-                 'last_post_date': x['last_post_date'], 
-                 'last_post_user_id': x['user_id'],
-                 'num_posts': x['num_posts']})
+        topics = []
+        if r.rowcount:
+            for x in r[offset:offset+limit]:
+                topics.append(
+                    {'topic_id': x['topic_id'],
+                     'last_post_id': x['last_post_id'], 
+                     'first_post_id': x['first_post_id'], 
+                     'group_id': x['group_id'], 
+                     'site_id': x['site_id'], 
+                     'subject': unicode(x['original_subject'], 'utf-8'), 
+                     'last_post_date': x['last_post_date'], 
+                     'last_post_user_id': x['user_id'],
+                     'num_posts': x['num_posts']})
+        
+        retval = (topics, r.rowcount, len(topics))
 
         return retval
     
