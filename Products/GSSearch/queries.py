973a1c4d4c2e9e8611817497328908231aa7d393
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-19 16:10:55 +1100

    Topic digests are now powered by gs.group.messages.topicsdigest

--- Products/GSSearch/queries.py
+++ Products/GSSearch/queries.py
@@ -1,5 +1,4 @@
 # coding=utf-8
 # Import the classes that used to be here!
 from queryfile import FileQuery  # lint:ok
-from querydigest import DigestQuery  # lint:ok
 from querymessage import MessageQuery  # lint:ok
